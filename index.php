3166f8770daaabdf1663f148f3506e6318a5adea
Randy <evilryok@gmail.com>
2017-07-24 13:17:12 +0800

    initial

---
+++ index.php
@@ -0,0 +1,24 @@
+¡<!DOCTYPE html>
+<html>
+<head>
+	<title>Leet Test</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+	<!-- js -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<div class="container">
+
+	<h1>Hello World</h1>
+
+	</div>
+
+
+</body>
+
+</html>
+
